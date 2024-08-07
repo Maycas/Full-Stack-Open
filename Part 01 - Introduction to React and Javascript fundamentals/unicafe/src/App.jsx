9e47e9b35edf51a22b277d9fe0fb1052ed0db3b1
Marc Maycas <marc.maycas@gmail.com>
2024-08-07 23:53:07 +0200

    Exercise 1.8

--- Part 01 - Introduction to React and Javascript fundamentals/unicafe/src/App.jsx
+++ Part 01 - Introduction to React and Javascript fundamentals/unicafe/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react';
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = ({ good, neutral, bad }) => {
   const totalFeedback = good + neutral + bad;
   const averageScore = totalFeedback !== 0 ? (good - bad) / totalFeedback : 0;
   const averagePositive =
     totalFeedback !== 0 ? (good / totalFeedback) * 100 : 0;
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {totalFeedback}</p>
+      <p>Average {averageScore}</p>
+      <p>Positive {averagePositive}%</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <>
@@ -19,15 +33,7 @@ const App = () => {
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
 
-      <div>
-        <h1>Statistics</h1>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>All {totalFeedback}</p>
-        <p>Average {averageScore}</p>
-        <p>Positive {averagePositive}%</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
