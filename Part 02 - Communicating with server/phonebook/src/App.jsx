fadd889c39109bd71db3a87368a11c0d71ed28cb
Marc Maycas <marc.maycas@gmail.com>
2024-08-16 02:01:57 +0200

    Considering ids in each person object

--- Part 02 - Communicating with server/phonebook/src/App.jsx
+++ Part 02 - Communicating with server/phonebook/src/App.jsx
@@ -3,14 +3,19 @@ import _ from 'lodash';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
-  const [newPerson, setNewPerson] = useState({ name: '', number: '' });
+  const [newPerson, setNewPerson] = useState({ name: '', number: '', id: '' });
 
   const addNewPerson = (event) => {
     event.preventDefault();
 
-    const personExists = persons.some((person) => _.isEqual(person, newPerson));
+    const personExists = persons.some((person) =>
+      _.isEqual(_.omit(person, ['id']), _.omit(newPerson, ['id']))
+    );
     if (personExists) {
       alert(
         `${newPerson.name} (${newPerson.number}) is already added to phonebook`
@@ -21,19 +26,23 @@ const App = () => {
   };
 
   const handleInputChange = (event) => {
-    const {name, value} = event.target
-    setNewPerson((prevPerson) => ({ ...prevPerson, [name]: value }));
-  }
+    const { name, value } = event.target;
+    setNewPerson((prevNewPerson) => ({
+      ...prevNewPerson,
+      [name]: value,
+      id: persons.length + 1,
+    }));
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input name="name" onChange={handleInputChange} />
+          name: <input name='name' onChange={handleInputChange} />
         </div>
         <div>
-          number: <input name="number" onChange={handleInputChange} />
+          number: <input name='number' onChange={handleInputChange} />
         </div>
         <div>
           <button type='submit' onClick={addNewPerson}>
@@ -42,8 +51,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-        <p key={index}>
+      {persons.map((person) => (
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
