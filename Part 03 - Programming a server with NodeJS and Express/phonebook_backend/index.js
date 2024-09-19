5d3738852d6790e9251554e2d67839a8b9f74fde
Marc Maycas <marc.maycas@gmail.com>
2024-09-19 13:04:24 +0200

    Exercise 3.3

--- Part 03 - Programming a server with NodeJS and Express/phonebook_backend/index.js
+++ Part 03 - Programming a server with NodeJS and Express/phonebook_backend/index.js
@@ -1,30 +1,8 @@
 import express from 'express';
+import persons from './data/persons.js';
 
 const app = express();
-const port = process.env.port || 3000;
-
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+const port = process.env.port || 3001;
 
 app.get('/info', (req, res) => {
   const date = new Date();
@@ -34,6 +12,21 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
 });
 
 app.listen(port, () => {
